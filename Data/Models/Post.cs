71de18f0964df2a53cd74c4b6fe73ad3b03e46b1
Forbzz <vital77717@gmail.com>
2021-11-11 12:45:27 +0300

    change architecture

---
+++ Data/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Post : BaseEntity
+    {
+        public string Content { get; set; }
+        public User Author { get; set; }
+        public string FileUrl { get; set; }
+        public IEnumerable<Like> Likes { get; set; }
+        public IEnumerable<Comment> Comments { get; set; }
+
+    }
+}
---
+++ Data/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class User : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string NickName { get; set; }
+        public uint Balance { get; set; }
+
+    }
+}
